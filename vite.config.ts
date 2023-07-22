fcb33085b0f3d24eb8d8fd190460bcfca8e30723
lonewanderer27 <adrianejames27@gmail.com>
2023-07-22 15:25:42 +0800

    increase PWA cache size

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     legacy(),
     VitePWA({
       registerType: "autoUpdate",
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5000000,
+      },
     }),
   ],
 });
